9d95d81419e3d2ae817ee5e858bd78fd29e7bf14
Masa Ivanov <masa.ivanov04.10@gmail.com>
2024-06-27 13:44:43 +0200

    Add list subscriptions lambda

---
+++ cloud-movies/lambdas/list_subscriptions/list_subscriptions.py
@@ -0,0 +1,30 @@
+import json
+import os
+import boto3
+from boto3.dynamodb.conditions import Key
+
+def handler(event, context):
+    dynamodb = boto3.resource('dynamodb')
+    
+    table_name = os.getenv('TABLE_NAME')
+    table = dynamodb.Table(table_name)
+
+    user_id = json.loads(event['pathParameters']['user_id'])
+    
+    response = table.query(
+        KeyConditionExpression=Key('user_id').eq(user_id)
+    )
+    
+    items = response.get('Items', [])
+    
+    while 'LastEvaluatedKey' in response:
+        response = table.query(
+            KeyConditionExpression=Key('user_id').eq(user_id),
+            ExclusiveStartKey=response['LastEvaluatedKey']
+        )
+        items.extend(response.get('Items', []))
+    
+    return {
+        'statusCode': 200,
+        'body': json.dumps(items)
+    }
